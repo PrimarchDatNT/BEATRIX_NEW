1a443cbec53a257ed43ba4b28a26772eb307fc9d
DatNT <datnt@segu.vn>
2023-03-19 18:54:36 +0700

    #15:  [Before change ids v1]

--- app/src/main/java/com/commsource/beautyplus/MainActivity.kt
+++ app/src/main/java/com/commsource/beautyplus/MainActivity.kt
@@ -2,7 +2,6 @@ package com.commsource.beautyplus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import  com.commsource.beautyplus.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
