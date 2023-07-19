a46fa3ce007d216c3d5c342c4f2a583298ed0b54
DatNT <datnt@segu.vn>
2023-07-19 19:02:52 +0700

    #commit[29]: [Remove][dir]: [Ln/d/a/a/ ]

---
+++ app/src/main/java/com/res/ANRTrace.java
@@ -0,0 +1,28 @@
+package com.res;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+
+import org.json.JSONObject;
+
+public final class ANRTrace {
+
+    private ANRTrace() {
+    }
+
+    public static void a(int i) {
+    }
+
+    @NonNull
+    public static JSONObject b(Application application, long j) {
+        return new JSONObject();
+    }
+
+    public static boolean c() {
+        return false;
+    }
+
+    public static void e(int i) {
+    }
+}
